e8aa9481f92d618d39f631df878ce3f31045a2fb
Nick Spyrison (Acer laptop) <spyrison@gmail.com>
2022-01-10 22:06:11 +1100

    -updates to use cases, videos, captions, and text. TODO: regression Use cases (not faceting correctly) & videos.

--- figures/_make_figures_case_study.r
+++ figures/_make_figures_case_study.r
@@ -6,8 +6,11 @@
   require("dplyr")
   require("ggplot2")
   require("cowplot")
+  require("patchwork")
   
-  setwd("~/R/cheem_paper")
+  wd <- getwd()
+  if(substr(wd, nchar(wd)-10, nchar(wd)) != "cheem_paper")
+    warning("Expected work directory to be cheem_paper.")
   fp <- "../cheem/inst/shiny_apps/cheem_initial/data/"
   if(F)
     dir("../cheem/inst/shiny_apps/cheem_initial/data/")
@@ -18,8 +21,9 @@
   ames2018_ls   <- readRDS(paste0(fp, "preprocess_ames2018.rds"))
   chocolates_ls <- readRDS(paste0(fp, "preprocess_chocolates.rds"))
   
-  .x_axis_title     <- "          x: PC1, y: PC2        x: PC1, y: PC2  x: predicted, y: observed"
-  .x_axis_title_reg <- "          x: PC1, y: PC2             x: PC1, y: PC2     x: predicted, y: observed"
+  .x_title      <- "        x: PC1, y: PC2          x: PC1, y: PC2  x: predicted, y: observed"
+  .x_title_reg  <- "      x: PC1, y: PC2               x: PC1, y: PC2     x: predicted, y: observed"
+  .x_title_reg2 <- "        x: PC1, y: PC2        x: PC1, y: PC2    x: predicted, y: observed"
 }
 
 ## Penguins classification ------
@@ -32,44 +36,58 @@
   ### Global view and cheem tour stills
   .glob_view <- global_view(
     penguins_ls, prim_obs, comp_obs, as_ggplot = TRUE) +
-    theme(legend.position = "right") +
-    labs(color = 'Predicted class', shape = 'Predicted class',
-         x = .x_axis_title) +
-    ggtitle("Global view")
+    labs(color = 'Predicted class', shape = 'Predicted class',x = .x_title) +
+    ggtitle("Global view") + theme( 
+      plot.margin      = margin(0,0,0,0),
+      legend.margin    = margin(0,0,0,0),
+      legend.position  = "bottom",
+      legend.direction = "horizontal")
   .bas <- basis_attr_df(penguins_ls$attr_df, prim_obs)
   .mv  <- 1 ## seems better than the 3 returned from
   ##manip_var_of_attr_df(penguins_ls$attr_df, prim_obs, comp_obs)
   ## Cheem tour for stills
-  .ggt <- radial_cheem_tour(
-    penguins_ls, basis = .bas, manip_var = .mv,
+  mt_interp <- manual_tour(basis = .bas, manip_var = .mv) %>% 
+    spinifex:::interpolate_manual_tour(angle = .15) ## app is .15
+  dim(mt_interp)
+  .ggt1 <- radial_cheem_tour(
+    penguins_ls, basis = mt_interp[,,1, drop=FALSE], manip_var = .mv,
     primary_obs = prim_obs, comparison_obs = comp_obs,
     do_add_pcp_segments = FALSE,
-    pcp_shape = 124, angle = 6) + theme(legend.position = "")
-  .cheem_stills <- spinifex::filmstrip(.ggt, nrow = 1) +
+    pcp_shape = 124, angle = 0) + 
+    theme(legend.position = "off", aspect.ratio = 2) +
     ggtitle("Radial tour, select frames")
+  .ggt2 <- radial_cheem_tour(
+    penguins_ls, basis = mt_interp[,,18, drop=FALSE], manip_var = .mv,
+    primary_obs = prim_obs, comparison_obs = comp_obs,
+    do_add_pcp_segments = FALSE,
+    pcp_shape = 124, angle = 0) + 
+    theme(legend.position = "off", aspect.ratio = 2)
+  ## Using patchwork:
+  .pw <- .ggt1 + .ggt2
   .cp <- cowplot::plot_grid(
-    .glob_view, .cheem_stills,
+    .glob_view, .pw,
     labels = c("a)", "b)"),
-    ncol = 1, rel_heights = c(1, 1.25))
+    ncol = 1, rel_heights = c(1, 1.6),
+    align = 'v', axis = 'l')
 }
 
 ### Save still shots for paper
 ggplot2::ggsave(
   "./figures/case_penguins.png",
   plot = .cp, device = "png",
-  width = 7, height = 6, units = "in")
+  width = 4.5, height = 7, units = "in")
 .m <- gc()
 
 ### Save .mp4, add GitHub urls to paper
 .ggt <- radial_cheem_tour(
   penguins_ls, basis = .bas, manip_var = .mv,
   primary_obs = prim_obs, comparison_obs = comp_obs,
-  do_add_pcp_segments = FALSE,
-  pcp_shape = 124, angle = .15) +## realistic angle
+  do_add_pcp_segments = TRUE,
+  pcp_shape = 124, angle = .15) + ## app angle
   theme(legend.position = "top", legend.direction = "horizontal")
 .anim <- animate_gganimate(
   .ggt, fps = 6, res = 100, ## resolution, not the same as dpi, 100 seems about 1x zoom
-  #height = 10, width = 12, units = "cm", ## "px", "in", "cm", or "mm."
+  height = 800, width = 600, units = "px", ## "px", "in", "cm", or "mm."
   render = gganimate::av_renderer("./figures/case_penguins.mp4")) ## Alternative render
 ## https://github.com/nspyrison/cheem_paper/blob/main/figures/case_penguins.mp4
 
@@ -81,49 +99,61 @@ ggplot2::ggsave(
   comp_obs <- 34L #"85% Cocoa Dark French Chocolate, Thorntos, UK (2nd) 
   if(F)
     global_view(chocolates_ls,
-                22, #"Dark Chocolate Bar, Lindt, Switzerland"
-                49) #"Pure Dark Chocolate, Hershey's, US"
+                prim_obs,
+                comp_obs)
   
   ### Global view
   .glob_view <- global_view(
     chocolates_ls, prim_obs, comp_obs, as_ggplot = TRUE) +
-    theme(legend.position = "right") +
-    labs(color = 'Predicted class', shape = 'Predicted class',
-         x = .x_axis_title) +
-    ggtitle("Global view")
-  .bas <- basis_attr_df(chocolates_ls$attr_df, prim_obs)
-  .mv  <- manip_var_of_attr_df(chocolates_ls$attr_df, prim_obs, comp_obs)
+    labs(color = 'Predicted class', shape = 'Predicted class',x = .x_title) +
+    ggtitle("Global view") + theme(
+      plot.margin      = margin(0,0,0,0),
+      legend.margin    = margin(0,0,0,0),
+      legend.position  = "bottom",
+      legend.direction = "horizontal")
+  .inc_var_nms <- c("Calories", "SatFat", "Chol", "Na", "Fiber", "Sugars")
+  ## Removed 4 with lowest contribution for the prim_obs.
+  .bas <- basis_attr_df(chocolates_ls$attr_df[, .inc_var_nms], prim_obs)
+  .mv  <- which(.inc_var_nms == "Sugars")
+  mt_interp <- manual_tour(.bas, .mv) %>%
+    spinifex:::interpolate_manual_tour(.15) ## App angle.
+  dim(mt_interp)
   ## Cheem tour stills for paper
-  .ggt <- radial_cheem_tour(
-    chocolates_ls, basis = .bas, manip_var = .mv,
+  .ggt1 <- radial_cheem_tour(
+    chocolates_ls, basis = mt_interp[,,1], manip_var = .mv,
     primary_obs = prim_obs, comparison_obs = comp_obs,
-    do_add_pcp_segments = FALSE,
-    pcp_shape = 124, angle = 6) + theme(legend.position = "")
-  .cheem_stills <- spinifex::filmstrip(.ggt, nrow = 1) +
+    do_add_pcp_segments = FALSE, inc_var_nms = .inc_var_nms,
+    pcp_shape = 124, angle = 0) + theme(legend.position = "off", aspect.ratio = 2) +
     ggtitle("Radial tour, select frames")
+  .ggt2 <- radial_cheem_tour(
+    chocolates_ls, basis = mt_interp[,,12], manip_var = .mv,
+    primary_obs = prim_obs, comparison_obs = comp_obs,
+    do_add_pcp_segments = FALSE, inc_var_nms = .inc_var_nms,
+    pcp_shape = 124, angle = 0) + theme(legend.position = "off", aspect.ratio = 2)
+  pw <- .ggt1 + .ggt2
   .cp <- cowplot::plot_grid(
-    .glob_view, .cheem_stills,
+    .glob_view, pw,
     labels = c("a)", "b)"),
-    ncol = 1, rel_heights = c(1, 1.25))
+    ncol = 1, rel_heights = c(1, 1.6))
 }
 
 ### Save Stills
 ggplot2::ggsave(
   "./figures/case_chocolates.png",
   plot = .cp, device = "png",
-  width = 6, height = 5, units = "in")
+  width = 5, height = 7, units = "in")
 .m <- gc()
 
 ### Save .mp4, add GitHub urls to paper
 .ggt <- radial_cheem_tour(
   chocolates_ls, basis = .bas, manip_var = .mv,
   primary_obs = prim_obs, comparison_obs = comp_obs,
-  do_add_pcp_segments = FALSE,
+  do_add_pcp_segments = TRUE, inc_var_nms = .inc_var_nms,
   pcp_shape = 124, angle = .15) + ## realistic angle
   theme(legend.position = "top", legend.direction = "horizontal")
 .anim <- animate_gganimate(
   .ggt, fps = 6, res = 100, ## resolution, not the same as dpi, 100 seems about 1x zoom
-  #height = 10, width = 12, units = "cm", ## "px", "in", "cm", or "mm."
+  height = 800, width = 600, units = "px", ## "px", "in", "cm", or "mm."
   render = gganimate::av_renderer("./figures/case_chocolates.mp4")) ## Alternative render
 ## https://github.com/nspyrison/cheem_paper/blob/main/figures/case_chocolates.mp4
 
@@ -137,24 +167,35 @@ ggplot2::ggsave(
   ### global view and tours
   .glob_view <- global_view(
     fifa_ls, prim_obs, comp_obs, as_ggplot = TRUE) +
-    theme(legend.position = "off") +
-    labs(x = .x_axis_title_reg) +
-    ggtitle("Global view")
-  ## Without reaction and movement
-  .bas <- basis_attr_df(fifa_ls$attr_df, prim_obs)[-c(3, 7),, drop=FALSE]
-  .mv  <- 3 ## "offense, that is what is talked about in the paper.
-  .ggt <- radial_cheem_tour(
-    fifa_ls, basis = .bas, manip_var = .mv,
+    labs(color = 'Predicted class', shape = 'Predicted class',x = .x_title_reg) +
+    ggtitle("Global view") + theme(
+      plot.margin      = margin(0,0,0,0),
+      legend.margin    = margin(0,0,0,0),
+      legend.position  = "bottom",
+      legend.direction = "horizontal")
+  .inc_var_nms <- c("BMI", "react", "off", "def", "mvm", "pwr")
+  ## Removed 4 with lowest contribution for the prim_obs.
+  .bas <- basis_attr_df(fifa_ls$attr_df[, .inc_var_nms], prim_obs)
+  .mv  <- which(.inc_var_nms == "def")
+  mt_interp <- manual_tour(.bas, .mv) %>%
+    spinifex:::interpolate_manual_tour(.15) ## App angle.
+  dim(mt_interp)
+  .ggt1 <- radial_cheem_tour(
+    fifa_ls, basis = mt_interp[,,1], manip_var = .mv,
     primary_obs = prim_obs, comparison_obs = comp_obs,
-    do_add_pcp_segments = FALSE,
-    pcp_shape = 124, angle = 6, inc_var_nms = colnames(fifa_ls$attr_df)[-c(3,7)]) +
-    theme(legend.position = "")
-  .cheem_stills <- spinifex::filmstrip(.ggt, nrow = 3) +
+    do_add_pcp_segments = FALSE, inc_var_nms = .inc_var_nms,
+    pcp_shape = 124, angle = 0) + theme(legend.position = "off", aspect.ratio = 1) +
     ggtitle("Radial tour, select frames")
+  .ggt2 <- radial_cheem_tour(
+    fifa_ls, basis = mt_interp[,,9], manip_var = .mv,
+    primary_obs = prim_obs, comparison_obs = comp_obs,
+    do_add_pcp_segments = FALSE, inc_var_nms = .inc_var_nms,
+    pcp_shape = 124, angle = 0) + theme(legend.position = "off", aspect.ratio = 1)
+  pw <- .ggt1 / .ggt2
   .cp <- cowplot::plot_grid(
-    .glob_view, .cheem_stills,
+    .glob_view, pw,
     labels = c("a)", "b)"),
-    ncol = 1, rel_heights = c(1, 2))
+    ncol = 1, rel_heights = c(1, 1.4))
 }
 ### Save
 ggplot2::ggsave(
@@ -167,12 +208,12 @@ ggplot2::ggsave(
 .ggt <- radial_cheem_tour(
   fifa_ls, basis = .bas, manip_var = .mv,
   primary_obs = prim_obs, comparison_obs = comp_obs,
-  do_add_pcp_segments = FALSE,
+  do_add_pcp_segments = TRUE, inc_var_nms = .inc_var_nms,
   pcp_shape = 124, angle = .15) + ## realistic angle
   theme(legend.position = "off")
 .anim <- animate_gganimate(
   .ggt, fps = 6, res = 100, ## resolution, not the same as dpi, 100 seems about 1x zoom
-  #height = 10, width = 12, units = "cm", ## "px", "in", "cm", or "mm."
+  height = 800, width = 800, units = "px", ## "px", "in", "cm", or "mm."
   render = gganimate::av_renderer("./figures/case_fifa.mp4")) ## Alternative render
 ## https://github.com/nspyrison/cheem_paper/blob/main/figures/case_fifa.mp4
 
@@ -180,39 +221,55 @@ ggplot2::ggsave(
 ## Ames Housing 2018 (North Ames) ----
 {
   names(ames2018_ls)
-  prim_obs <- 170L
-  comp_obs <- 220L
+  prim_obs <- 311L ## Large lot area to living area ratio ## small house on big lot
+  comp_obs <- 220L ## Small on that ratio ## large house small lot
   if(F)
     global_view(ames2018_ls, prim_obs, comp_obs,
                 color = ames2018_ls$decode_df$residual,
                 shape = factor(ames2018_ls$decode_df$class))
   
-  ### global view
   .glob_view <- global_view(
     ames2018_ls, prim_obs, comp_obs, as_ggplot = TRUE) +
-    theme(legend.position = "off") +
-    labs(x = .x_axis_title_reg) +
-    ggtitle("Global view")
-  .bas <- basis_attr_df(ames2018_ls$attr_df, prim_obs)
-  .mv  <- manip_var_of_attr_df(ames2018_ls$attr_df, prim_obs, comp_obs)
-  ### Cheem toru still shots
-  .ggt <- radial_cheem_tour(
-    ames2018_ls, basis = .bas, manip_var = .mv,
+    labs(color = 'Predicted class', shape = 'Predicted class',x = .x_title_reg) +
+    ggtitle("Global view") + theme(
+      plot.margin      = margin(0,0,0,0),
+      legend.margin    = margin(0,0,0,0),
+      legend.position  = "bottom",
+      legend.direction = "horizontal")
+  .inc_var_nms <- c("LtA", "Qlt", "LvA", "Rms", "GYB", "GrA")
+  ## Removed 4 with lowest contribution for the prim_obs.
+  .bas <- basis_attr_df(ames2018_ls$attr_df[, .inc_var_nms], prim_obs)
+  .mv  <- which(.inc_var_nms == "LvA")
+  mt_interp <- manual_tour(.bas, .mv) %>%
+    spinifex:::interpolate_manual_tour(.15) ## App angle.
+  dim(mt_interp)
+  .ggt1 <- radial_cheem_tour(
+    ames2018_ls, basis = mt_interp[,,1], manip_var = .mv,
     primary_obs = prim_obs, comparison_obs = comp_obs,
-    do_add_pcp_segments = FALSE,
-    pcp_shape = 124, angle = 6) + theme(legend.position = "")
-  .cheem_stills <- spinifex::filmstrip(.ggt, nrow = 3) +
+    do_add_pcp_segments = FALSE, inc_var_nms = .inc_var_nms,
+    pcp_shape = 124, angle = 0) + theme(legend.position = "off", aspect.ratio = 1) +
     ggtitle("Radial tour, select frames")
+  # .ggt2 <- radial_cheem_tour( ## Max difference
+  #   ames2018_ls, basis = mt_interp[,,8], manip_var = .mv,
+  #   primary_obs = prim_obs, comparison_obs = comp_obs,
+  #   do_add_pcp_segments = FALSE, inc_var_nms = .inc_var_nms,
+  #   pcp_shape = 124, angle = 0) + theme(legend.position = "off", aspect.ratio = 1)
+  .ggt3 <- radial_cheem_tour(
+    ames2018_ls, basis = mt_interp[,,19], manip_var = .mv,
+    primary_obs = prim_obs, comparison_obs = comp_obs,
+    do_add_pcp_segments = FALSE, inc_var_nms = .inc_var_nms,
+    pcp_shape = 124, angle = 0) + theme(legend.position = "off", aspect.ratio = 1)
+  pw <- .ggt1 / .ggt3
   .cp <- cowplot::plot_grid(
-    .glob_view, .cheem_stills,
+    .glob_view, pw,
     labels = c("a)", "b)"),
-    ncol = 1, rel_heights = c(1, 2.4))
+    ncol = 1, rel_heights = c(1, 1.4))
 }
 ### Save
 ggplot2::ggsave(
   "./figures/case_ames2018.png",
   plot = .cp, device = "png",
-  width = 5, height = 8, units = "in")
+  width = 6, height = 8, units = "in")
 .m <- gc()
 
 ### Save .mp4, add GitHub urls to paper
